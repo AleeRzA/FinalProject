2e9e81f94504a0bc4e71c6eed834117dbe06c307
alirezanasr <alireza_nasrollahi@yahoo.com>
2019-02-10 19:09:04 +0330

    SplashScreen created

--- app/src/main/java/com/example/android/finalproject/MainActivity.java
+++ app/src/main/java/com/example/android/finalproject/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.android.finalproject;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static Intent newIntent(Context context){
+        Intent intent = new Intent(context, MainActivity.class);
+        return intent;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
